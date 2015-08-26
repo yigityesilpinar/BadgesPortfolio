c8dd4eeb275fdabba1c2e6c63742839e8a69494f
23yesil <yigityesilpinar@gmail.com>
2015-08-26 14:48:45 +0200

    Js Fix and Optimization, and more
    Js file is optimazed as functions, ready css created for levels 'A1,A2
    ...'

--- public/js/badge-portfolio-public.js
+++ public/js/badge-portfolio-public.js
@@ -4,6 +4,94 @@ jQuery(document).ready(function ($) {
     var slashes = http.concat("//");
     var host = slashes.concat(window.location.hostname);
     var plugin_url = host.concat('/wp-content/plugins/BadgePortfolio/');
+    function displayForm(data1) {
+        var skills_div = '';
+        var data = data1[0];
+        var skill = data1[1];
+        var ans;
+        var level;
+        var learn_lang = $('#langsAutocompSkills').val();
+        var skills = ["Writing", "Interaction", "Reading", "Listening", "Speaking"];
+        if (level) {
+            var newTxt = learn_lang.split('(');
+            for (var i = 1; i < newTxt.length; i++) {
+
+                if (newTxt[i].split(')')[0].length == 3) {
+                    learn_lang = newTxt[i].split(')')[0];
+                }
+            }
+            for (i = 0; i < skills.length; i++) {
+                if (i != skill) {
+                    skills_div += '<a href="' + i + '">' + skills[i] + "</a>";
+                }
+                else {
+                    skills_div += '<a href="' + i + '" class="selected">' + skills[i] + "</a>";
+                }
+            }
+            var result = '';
+            result += '<div id="skills_delete_record_div"><a href="' + learn_lang + '">Delete this Record</a></div>';
+            result += '<div id="skills_selected_lang_div"> Your current level: ' + level + '</div>';
+        }
+        else {
+            var result = '';
+        }
+        for (i = 0; i < skills.length; i++) {
+            if (i != skill) {
+                skills_div += '<a href="' + i + '">' + skills[i] + "</a>";
+            }
+            else {
+                skills_div += '<a href="' + i + '" class="selected">' + skills[i] + "</a>";
+            }
+        }
+        var checked = '';
+        var count = 0;
+        result += '<table style="width:100%;">';
+        result += ' <tr><th width="70%">Questions</th><th width="30%">YES MAYBE NO</th></tr>';
+        for (var key in data) {
+            if (data.hasOwnProperty(key)) {
+
+                var level = data[key].level;
+                var questions = data[key].questions;
+
+                for (var key2 in questions) {
+                    if (questions.hasOwnProperty(key2)) {
+
+                        var question = questions[key2];
+                        result += '<tr><td class="' + level + '">';
+                        result += question;
+                        result += '</td><td class="' + level + '">';
+                        checked = '';
+                        count = 0;
+                        if (ans && ans[count] === 'y')
+                            checked = "checked='checked'";
+                        result += "<input type='radio' name='skills_radio_" + level + '_' + count + "' value='y'" + checked + "  />";
+                        count++;
+                        checked = '';
+                        if (ans && ans[count] === 'm')
+                            checked = "checked='checked'";
+                        result += "<input type='radio' name='skills_radio_" + level + '_' + count + "' value='m'" + checked + "  />";
+                        count++;
+                        checked = '';
+                        if (ans && ans[count] === 'n')
+                            checked = "checked='checked'";
+                        result += "<input type='radio' name='skills_radio_" + level + '_' + count + "' value='n'" + checked + "  />";
+
+                        result += '</td></tr>';
+                        count++;
+                    }
+                }
+
+            }
+        }
+        result += '</table>' + '<p><input type="submit" name="skills_form_save" value="Save"/></p>';
+        $("#skills_div").html(skills_div);
+        $("#skills_questions_div").html(result);
+
+
+    }
+
+
+
     $('#langsAutocompSkills').autocomplete({
         source: plugin_url + 'includes/partials/badge-portfolio-langs.php',
         minLength: 2,
@@ -20,87 +108,8 @@ jQuery(document).ready(function ($) {
                     'skill': skill
                 },
                 dataType: 'json',
-                success: function (data1) {
-                    var skills_div = '';
-                    var data = data1[0];
-                    var skill = data1[1];
-                    var ans;
-                    var level;
-                    var learn_lang = $('#langsAutocompSkills').val();
-                    var skills = ["Writing", "Interaction", "Reading", "Listening", "Speaking"];
-                    if (level) {
-                        var newTxt = learn_lang.split('(');
-                        for (var i = 1; i < newTxt.length; i++) {
-
-                            if (newTxt[i].split(')')[0].length == 3) {
-                                learn_lang = newTxt[i].split(')')[0];
-                            }
-                        }
-                        for (i = 0; i < skills.length; i++) {
-                            if (i != skill) {
-                                skills_div += '<a href="' + i + '">' + skills[i] + "</a>";
-                            }
-                            else {
-                                skills_div += '<a href="' + i + '" class="selected">' + skills[i] + "</a>";
-                            }
-                        }
-                        var result = '';
-                        result += '<div id="skills_delete_record_div"><a href="' + learn_lang + '">Delete this Record</a></div>';
-                        result += '<div id="skills_selected_lang_div"> Your current level: ' + level + '</div>';
-                    }
-                    else {
-                        var result = '';
-                    }
-                    for (i = 0; i < skills.length; i++) {
-                        if (i != skill) {
-                            skills_div += '<a href="' + i + '">' + skills[i] + "</a>";
-                        }
-                        else {
-                            skills_div += '<a href="' + i + '" class="selected">' + skills[i] + "</a>";
-                        }
-                    }
-                    var checked = '';
-                    var count = 0;
-                    result += '<table style="width:100%;">';
-                    result += ' <tr><th width="70%">Questions</th><th width="30%">YES MAYBE NO</th></tr>';
-                    for (var key in data) {
-                        if (data.hasOwnProperty(key)) {
-
-                            var level = data[key].level;
-                            var questions = data[key].questions;
-
-                            for (var key2 in questions) {
-                                if (questions.hasOwnProperty(key2)) {
-
-                                    var question = questions[key2];
-                                    result += '<tr><td class="' + level + '">';
-                                    result += question;
-                                    result += '</td><td>';
-                                    checked = '';
-                                    if (ans && ans[count] === 'y')
-                                        checked = "checked='checked'";
-                                    result += "<input type='radio' name='skills_radio_" + level + '_' + count + "' value='y'" + checked + "  />";
-                                    checked = '';
-                                    if (ans && ans[count] === 'm')
-                                        checked = "checked='checked'";
-                                    result += "<input type='radio' name='skills_radio_" + level + '_' + count + "' value='m'" + checked + "  />";
-                                    checked = '';
-                                    if (ans && ans[count] === 'n')
-                                        checked = "checked='checked'";
-                                    result += "<input type='radio' name='skills_radio_" + level + '_' + count + "' value='n'" + checked + "  />";
-
-                                    result += '</td></tr>';
-                                    count++;
-                                }
-                            }
-
-                        }
-                    }
-                    result += '</table>' + '<p><input type="submit" name="skills_form_save" value="Save"/></p>';
-                    $("#skills_div").html(skills_div);
-                    $("#skills_questions_div").html(result);
-
-
+                success: function (data) {
+                    displayForm(data);
                 }
             });
         },
@@ -135,87 +144,8 @@ jQuery(document).ready(function ($) {
                 'skill': skill
             },
             dataType: 'json',
-            success: function (data1) {
-                var skills_div = '';
-                var data = data1[0];
-                var skill = data1[1];
-                var ans;
-                var level;
-                var learn_lang = $('#langsAutocompSkills').val();
-                var skills = ["Writing", "Interaction", "Reading", "Listening", "Speaking"];
-                if (level) {
-                    var newTxt = learn_lang.split('(');
-                    for (var i = 1; i < newTxt.length; i++) {
-
-                        if (newTxt[i].split(')')[0].length == 3) {
-                            learn_lang = newTxt[i].split(')')[0];
-                        }
-                    }
-                    for (i = 0; i < skills.length; i++) {
-                        if (i != skill) {
-                            skills_div += '<a href="' + i + '">' + skills[i] + "</a>";
-                        }
-                        else {
-                            skills_div += '<a href="' + i + '" class="selected">' + skills[i] + "</a>";
-                        }
-                    }
-                    var result = '';
-                    result += '<div id="skills_delete_record_div"><a href="' + learn_lang + '">Delete this Record</a></div>';
-                    result += '<div id="skills_selected_lang_div"> Your current level: ' + level + '</div>';
-                }
-                else {
-                    var result = '';
-                }
-                for (i = 0; i < skills.length; i++) {
-                    if (i != skill) {
-                        skills_div += '<a href="' + i + '">' + skills[i] + "</a>";
-                    }
-                    else {
-                        skills_div += '<a href="' + i + '" class="selected">' + skills[i] + "</a>";
-                    }
-                }
-                var checked = '';
-                var count = 0;
-                result += '<table style="width:100%;">';
-                result += ' <tr><th width="70%">Questions</th><th width="30%">YES MAYBE NO</th></tr>';
-                for (var key in data) {
-                    if (data.hasOwnProperty(key)) {
-
-                        var level = data[key].level;
-                        var questions = data[key].questions;
-
-                        for (var key2 in questions) {
-                            if (questions.hasOwnProperty(key2)) {
-
-                                var question = questions[key2];
-                                result += '<tr><td class="' + level + '">';
-                                result += question;
-                                result += '</td><td>';
-                                checked = '';
-                                if (ans && ans[count] === 'y')
-                                    checked = "checked='checked'";
-                                result += "<input type='radio' name='skills_radio_" + level + '_' + count + "' value='y'" + checked + "  />";
-                                checked = '';
-                                if (ans && ans[count] === 'm')
-                                    checked = "checked='checked'";
-                                result += "<input type='radio' name='skills_radio_" + level + '_' + count + "' value='m'" + checked + "  />";
-                                checked = '';
-                                if (ans && ans[count] === 'n')
-                                    checked = "checked='checked'";
-                                result += "<input type='radio' name='skills_radio_" + level + '_' + count + "' value='n'" + checked + "  />";
-
-                                result += '</td></tr>';
-                                count++;
-                            }
-                        }
-
-                    }
-                }
-                result += '</table>' + '<p><input type="submit" name="skills_form_save" value="Save"/></p>';
-                $("#skills_div").html(skills_div);
-                $("#skills_questions_div").html(result);
-
-
+            success: function (data) {
+                displayForm(data);
             }
         });
 
@@ -243,87 +173,8 @@ jQuery(document).ready(function ($) {
                 'skill': skill
             },
             dataType: 'json',
-            success: function (data1) {
-                var skills_div = '';
-                var data = data1[0];
-                var skill = data1[1];
-                var ans;
-                var level;
-                var learn_lang = $('#langsAutocompSkills').val();
-                var skills = ["Writing", "Interaction", "Reading", "Listening", "Speaking"];
-                if (level) {
-                    var newTxt = learn_lang.split('(');
-                    for (var i = 1; i < newTxt.length; i++) {
-
-                        if (newTxt[i].split(')')[0].length == 3) {
-                            learn_lang = newTxt[i].split(')')[0];
-                        }
-                    }
-                    for (i = 0; i < skills.length; i++) {
-                        if (i != skill) {
-                            skills_div += '<a href="' + i + '">' + skills[i] + "</a>";
-                        }
-                        else {
-                            skills_div += '<a href="' + i + '" class="selected">' + skills[i] + "</a>";
-                        }
-                    }
-                    var result = '';
-                    result += '<div id="skills_delete_record_div"><a href="' + learn_lang + '">Delete this Record</a></div>';
-                    result += '<div id="skills_selected_lang_div"> Your current level: ' + level + '</div>';
-                }
-                else {
-                    var result = '';
-                }
-                for (i = 0; i < skills.length; i++) {
-                    if (i != skill) {
-                        skills_div += '<a href="' + i + '">' + skills[i] + "</a>";
-                    }
-                    else {
-                        skills_div += '<a href="' + i + '" class="selected">' + skills[i] + "</a>";
-                    }
-                }
-                var checked = '';
-                var count = 0;
-                result += '<table style="width:100%;">';
-                result += ' <tr><th width="70%">Questions</th><th width="30%">YES MAYBE NO</th></tr>';
-                for (var key in data) {
-                    if (data.hasOwnProperty(key)) {
-
-                        var level = data[key].level;
-                        var questions = data[key].questions;
-
-                        for (var key2 in questions) {
-                            if (questions.hasOwnProperty(key2)) {
-
-                                var question = questions[key2];
-                                result += '<tr><td class="' + level + '">';
-                                result += question;
-                                result += '</td><td>';
-                                checked = '';
-                                if (ans && ans[count] === 'y')
-                                    checked = "checked='checked'";
-                                result += "<input type='radio' name='skills_radio_" + level + '_' + count + "' value='y'" + checked + "  />";
-                                checked = '';
-                                if (ans && ans[count] === 'm')
-                                    checked = "checked='checked'";
-                                result += "<input type='radio' name='skills_radio_" + level + '_' + count + "' value='m'" + checked + "  />";
-                                checked = '';
-                                if (ans && ans[count] === 'n')
-                                    checked = "checked='checked'";
-                                result += "<input type='radio' name='skills_radio_" + level + '_' + count + "' value='n'" + checked + "  />";
-
-                                result += '</td></tr>';
-                                count++;
-                            }
-                        }
-
-                    }
-                }
-                result += '</table>' + '<p><input type="submit" name="skills_form_save" value="Save"/></p>';
-                $("#skills_div").html(skills_div);
-                $("#skills_questions_div").html(result);
-
-
+            success: function (data) {
+                displayForm(data);
             }
         });
     });
